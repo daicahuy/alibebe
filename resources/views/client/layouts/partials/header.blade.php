331499800e9477977542c80976b03b8e79b6cd8b
daicahuy <huymamicoi@gmail.com>
2025-04-23 11:15:12 +0700

    huynh_fix_something

--- resources/views/client/layouts/partials/header.blade.php
+++ resources/views/client/layouts/partials/header.blade.php
@@ -589,13 +589,6 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="header-nav-right">
-                        <button class="btn deal-button" data-bs-toggle="modal" data-bs-target="#deal-box">
-                            <i data-feather="zap"></i>
-                            <span>Deal Today</span>
-                        </button>
-                    </div>
                 </div>
             </div>
         </div>
