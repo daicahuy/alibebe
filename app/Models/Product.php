01390b350254b795a8624f91c142ccc4ca2c3b11
tungle172004 <letung172004@gmail.com>
2025-05-01 00:08:37 +0700

    Tungld_fix_100%

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -179,7 +179,7 @@ class Product extends Model
                 'products.thumbnail',
                 'products.views'
             )
-            ->orderByDesc('products.updated_at')
+            ->orderByDesc('products.View')
             ->limit(10);
     }
 
